ee2823bb5c94b8e0a0ac4b1f6b20528445e47c74
JadhavC07 <cjaddhav@gmail.com>
2024-09-08 14:41:44 +0530

    updated seo

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import { TypeAnimation } from "react-type-animation";
 import { useEffect } from "react";
 import Aos from "aos";
 import "aos/dist/aos.css";
-
+import Image from "next/image";
 
 const Home = () => {
   useEffect(() => {
@@ -13,74 +13,79 @@ const Home = () => {
 
   return (
     <>
- 
-      <div className="flex justify-center py-12 bg-white dark:bg-gray-950">
+      
+      <header className="flex justify-center py-12 bg-white dark:bg-gray-950">
         <div className="text-center p-20">
-          <h1 className="text-4xl font-extrabold leading-5 mb-4 roboto-regula text-lime-500 ml11">
-            <div className="text-5xl font-bold">
-              <TypeAnimation
-                preRenderFirstString={true}
-                sequence={[
-                  500,
-                  "I'm a Web Developer",
-                  1000,
-                  "I'm a Front-End Developer",
-                  1000,
-                  "I'm a Back-End Developer",
-                  1000,
-                  "I'm a Full Stack Developer",
-                  500,
-                ]}
-                speed={50}
-                repeat={Infinity}
-              />
-            </div>
+          <h1 className="text-5xl font-bold text-lime-500">
+            <TypeAnimation
+              preRenderFirstString={true}
+              sequence={[
+                500,
+                "I'm a Web Developer",
+                1000,
+                "I'm a Front-End Developer",
+                1000,
+                "I'm a Back-End Developer",
+                1000,
+                "I'm a Full Stack Developer",
+                500,
+              ]}
+              speed={50}
+              repeat={Infinity}
+            />
           </h1>
-          <h1 className="text-4xl font-bold mb-4 roboto-regula text-lime-500">
+          <h2 className="text-4xl font-bold mb-4 text-lime-500">
             <TypeAnimation
               sequence={["ALL", 500, "IN", 500, "ONE", 500]}
               repeat={Infinity}
             />
-          </h1>
-          <h2 className="text-2xl mb-4 home-font text-lime-500 opacity-95 roboto-regular">
+          </h2>
+          <p className="text-2xl mb-4 text-lime-500 opacity-95">
             &quot;Designing the future, one line of code at a time. Your
             full-stack maestro of innovation and elegance.&quot;
-          </h2>
+          </p>
 
-          <div className="w-48 h-58 mx-auto rounded-full overflow-hidden">
-            <img
+          <figure className="w-48 h-58 mx-auto rounded-full overflow-hidden">
+            <Image
               className="w-full h-full object-cover"
               style={{ width: "210px" }}
               src="/Images/my-prop.svg"
-              alt="Profile"
+              alt="Chandan Jadhav Full Stack Developer Profile Picture"
+              loading="lazy"
+            />
+          </figure>
+        </div>
+      </header>
+
+  
+      <main className="dark:bg-gray-950">
+        <div className="container mx-auto text-center">
+          <div className="inline-block relative" style={{ maxWidth: "860px" }}>
+            <Image
+              src="/Images/my-pc.svg"
+              alt="Chandan Jadhav Full Stack Developer Work Setup"
+              loading="lazy"
             />
           </div>
         </div>
-      </div>
+      </main>
 
-      <div className="dark:bg-gray-950">
-        <div className="container mx-auto text-center">
-          <div className="inline-block relative" style={{ maxWidth: "860px" }}>
-            <img src="/Images/my-pc.svg" alt="SVG Image" />
-          </div>
-        </div>
-      </div>
+
       <section
         className="text-black text-center py-16"
         style={{ backgroundColor: "rgb(58, 255, 9)" }}
       >
         <div className="container mx-auto">
-          <div className="flex justify-center">
+          <article className="flex justify-center">
             <div className="w-3/5">
-              <h1 className="text-3xl md:text-4xl font-bold mb-4">
+              <h2 className="text-3xl md:text-4xl font-bold mb-4">
                 Hi, I’m Chandan. Nice to meet you.
-              </h1>
-              <h2 className="text-xl md:text-xl font-medium">
-                With 6 months of internship experience and now serving as a
-                junior software developer...
               </h2>
+              <p className="text-xl md:text-xl font-medium">
+                With 6 months of internship experience and now serving as a junior software developer...
+              </p>
             </div>
-          </div>
+          </article>
         </div>
       </section>
     </>
--- src/app/portfolio/contact/page.jsx
+++ src/app/portfolio/contact/page.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import emailjs from "@emailjs/browser";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-
+import { FaEnvelope } from "react-icons/fa";
 const Contact = () => {
   const [formData, setFormData] = useState({
     name: "",
@@ -17,32 +17,20 @@ const Contact = () => {
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
-    setErrors({ ...errors, [name]: "" }); 
+    setErrors({ ...errors, [name]: "" });
   };
 
   const validateForm = () => {
     const newErrors = {};
 
-    if (!formData.name.trim()) {
-      newErrors.name = "Name is required";
-    }
-
-    if (!formData.email.trim()) {
-      newErrors.email = "Email is required";
-    } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
+    if (!formData.name.trim()) newErrors.name = "Name is required";
+    if (!formData.email.trim()) newErrors.email = "Email is required";
+    else if (!/\S+@\S+\.\S+/.test(formData.email))
       newErrors.email = "Email is invalid";
-    }
-
-    if (!formData.subject.trim()) {
-      newErrors.subject = "Subject is required";
-    }
-
-    if (!formData.message.trim()) {
-      newErrors.message = "Message is required";
-    }
+    if (!formData.subject.trim()) newErrors.subject = "Subject is required";
+    if (!formData.message.trim()) newErrors.message = "Message is required";
 
     setErrors(newErrors);
-
     return Object.keys(newErrors).length === 0;
   };
 
@@ -57,135 +45,133 @@ const Contact = () => {
           formData,
           "3g7fmoqhPZJraRgJ4"
         );
-        setFormData({
-          name: "",
-          email: "",
-          subject: "",
-          message: "",
-        });
-        // Handle successful form submission
-        console.log("Form submitted successfully");
-        toast.success("Form submitted successfully", {
-          position: "top-center",
-        });
+        setFormData({ name: "", email: "", subject: "", message: "" });
+        toast.success("Form sent successfully", { position: "top-center" });
       } catch (error) {
-        toast.error("Error submitting form", {
-          position: "top-center",
-        });
-
-        // Handle error
+        toast.error("Error submitting form", { position: "top-center" });
         console.error("Error submitting form:", error);
       }
     }
   };
 
   return (
-    <div className="min-h-screen bg-gray-100 dark:bg-gray-950 flex justify-center items-center">
-      <div className="bg-white dark:bg-gray-950 p-8 rounded-lg border dark:border-lime-500  shadow-md w-full sm:max-w-lg">
+    <div className="min-h-screen bg-gray-100 dark:bg-gray-950 flex justify-center items-center p-4">
+      <div className="bg-white dark:bg-gray-900 p-8 rounded-lg border dark:border-lime-500 shadow-lg w-full max-w-6xl grid grid-cols-1 md:grid-cols-2 gap-8">
+        {/* Left Side - Contact Form */}
         <div className="flex flex-col">
-          <div>
-            <h2 className="text-2xl dark:text-white text-lime-400 font-semibold mb-4">
-              Contact Me
-            </h2>
-          </div>
-          <div>
-            <ToastContainer />
-          </div>
+          <h2 className="text-2xl dark:text-white text-lime-400 font-semibold mb-4">
+            Contact Me
+          </h2>
+          <form onSubmit={handleSubmit}>
+            <div className="mb-4">
+              <label
+                htmlFor="name"
+                className="block text-gray-600 dark:text-white font-medium mb-2"
+              >
+                Name
+              </label>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                value={formData.name}
+                onChange={handleChange}
+                className={`w-full px-4 py-2 border rounded-md focus:outline-none ${
+                  errors.name && "border-red-500"
+                }`}
+              />
+              {errors.name && (
+                <p className="text-red-500 text-sm mt-1">{errors.name}</p>
+              )}
+            </div>
+            <div className="mb-4">
+              <label
+                htmlFor="email"
+                className="block text-gray-600 dark:text-white font-medium mb-2"
+              >
+                Email
+              </label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                className={`w-full px-4 py-2 border rounded-md focus:outline-none ${
+                  errors.email && "border-red-500"
+                }`}
+              />
+              {errors.email && (
+                <p className="text-red-500 text-sm mt-1">{errors.email}</p>
+              )}
+            </div>
+            <div className="mb-4">
+              <label
+                htmlFor="subject"
+                className="block text-gray-600 dark:text-white font-medium mb-2"
+              >
+                Subject
+              </label>
+              <input
+                type="text"
+                id="subject"
+                name="subject"
+                value={formData.subject}
+                onChange={handleChange}
+                className={`w-full px-4 py-2 border rounded-md focus:outline-none ${
+                  errors.subject && "border-red-500"
+                }`}
+              />
+              {errors.subject && (
+                <p className="text-red-500 text-sm mt-1">{errors.subject}</p>
+              )}
+            </div>
+            <div className="mb-6">
+              <label
+                htmlFor="message"
+                className="block text-gray-600 dark:text-white font-medium mb-2"
+              >
+                Message
+              </label>
+              <textarea
+                id="message"
+                name="message"
+                value={formData.message}
+                onChange={handleChange}
+                rows="4"
+                className={`w-full px-4 py-2 border rounded-md focus:outline-none ${
+                  errors.message && "border-red-500"
+                }`}
+              ></textarea>
+              {errors.message && (
+                <p className="text-red-500 text-sm mt-1">{errors.message}</p>
+              )}
+            </div>
+            <div className="flex justify-end">
+              <button
+                type="submit"
+                className="px-6 py-2 bg-lime-500 dark:bg-lime-500 text-white rounded-md hover:bg-lime-700 focus:outline-none"
+              >
+                Submit
+              </button>
+            </div>
+          </form>
+          <ToastContainer />
         </div>
-        <form onSubmit={handleSubmit}>
-          <div className="mb-4">
-            <label
-              htmlFor="name"
-              className="block text-gray-600 dark:text-white font-medium mb-2"
-            >
-              Name
-            </label>
-            <input
-              type="text"
-              id="name"
-              name="name"
-              value={formData.name}
-              onChange={handleChange}
-              className={`w-full px-4 py-2 border rounded-md focus:outline-none ${
-                errors.name && "border-red-500"
-              }`}
-            />
-            {errors.name && (
-              <p className="text-red-500 text-sm mt-1">{errors.name}</p>
-            )}
-          </div>
-          <div className="mb-4">
-            <label
-              htmlFor="email"
-              className="block text-gray-600 dark:text-white font-medium mb-2"
-            >
-              Email
-            </label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              value={formData.email}
-              onChange={handleChange}
-              className={`w-full px-4 py-2 border rounded-md focus:outline-none ${
-                errors.email && "border-red-500"
-              }`}
-            />
-            {errors.email && (
-              <p className="text-red-500 text-sm mt-1">{errors.email}</p>
-            )}
-          </div>
-          <div className="mb-4">
-            <label
-              htmlFor="subject"
-              className="block text-gray-600 dark:text-white font-medium mb-2"
-            >
-              Subject
-            </label>
-            <input
-              type="text"
-              id="subject"
-              name="subject"
-              value={formData.subject}
-              onChange={handleChange}
-              className={`w-full px-4 py-2 border rounded-md focus:outline-none ${
-                errors.subject && "border-red-500"
-              }`}
-            />
-            {errors.subject && (
-              <p className="text-red-500 text-sm mt-1">{errors.subject}</p>
-            )}
-          </div>
-          <div className="mb-6">
-            <label
-              htmlFor="message"
-              className="block text-gray-600 dark:text-white font-medium mb-2"
-            >
-              Message
-            </label>
-            <textarea
-              id="message"
-              name="message"
-              value={formData.message}
-              onChange={handleChange}
-              rows="4"
-              className={`w-full px-4 py-2 border rounded-md focus:outline-none ${
-                errors.message && "border-red-500"
-              }`}
-            ></textarea>
-            {errors.message && (
-              <p className="text-red-500 text-sm mt-1">{errors.message}</p>
-            )}
-          </div>
-          <div className="flex justify-end">
-            <button
-              type="submit"
-              className="px-6 py-2 bg-lime-500 dark:bg-lime-900 text-white rounded-md hover:bg-lime-700 focus:outline-none focus:bg-indigo-600"
-            >
-              Submit
-            </button>
-          </div>
-        </form>
+
+        {/* Right Side - Image/ Icon */}
+<div className="flex flex-col justify-center items-center text-center space-y-4">
+  <FaEnvelope className="text-8xl text-lime-500 dark:text-lime-400" />
+  
+  <h1 className="text-2xl font-bold dark:text-white text-lime-400">
+    Let&apos;s Build Something Amazing Together!
+  </h1>
+  
+  <p className="text-gray-600 dark:text-gray-300 max-w-md">
+    Got a project in mind or just want to connect? Whether you need a full-stack solution, collaboration on a challenging tech project, or have any questions about my work, I’m here to help. Feel free to drop me a message, and I’ll get back to you as soon as possible!
+  </p>
+</div>
+
       </div>
     </div>
   );
