35b160fb79241ebf43e90615195467ed961f8f02
Chandan Jadhav <119391943+JadhavC07@users.noreply.github.com>
2024-09-14 12:42:04 +0000

    added blog page

--- src/app/components/TransitionEffect.js
+++ src/app/components/TransitionEffect.js
@@ -7,21 +7,21 @@ const TransitionEffect = () => {
         className="fixed top-full bottom-0 right-0 w-screen h-screen z-30 bg-lime-600"
         initial={{ y: "100%", width: "100%" }}
         animate={{ y: "0%", width: "0%" }}
-        exit={{ x: ["0%", "100%"], width: ["0%", "100%"] }}
+        exit={{ y: ["0%", "100%"], width: ["0%", "100%"] }}
         transition={{ duration: 0.8, ease: "easeInOut" }}
       />
       <motion.div
         className="fixed top-full bottom-0 right-0 w-screen h-screen z-20 bg-lime-500"
         initial={{ y: "100%", width: "100%" }}
         animate={{ y: "0%", width: "0%" }}
-        exit={{ x: ["0%", "100%"], width: ["0%", "100%"] }}
+        exit={{ y: ["0%", "100%"], width: ["0%", "100%"] }}
         transition={{ delay: 0.2, duration: 0.8, ease: "easeInOut" }}
       />
       <motion.div
         className="fixed top-full bottom-0 right-0 w-screen h-screen z-10 bg-lime-300"
         initial={{ y: "100%", width: "100%" }}
         animate={{ y: "0%", width: "0%" }}
-        exit={{ x: ["0%", "100%"], width: ["0%", "100%"] }}
+        exit={{ y: ["0%", "100%"], width: ["0%", "100%"] }}
         transition={{ delay: 0.4, duration: 0.8, ease: "easeInOut" }}
       />
     </>
