f7fb43049c12328a50e1eeb413c7e913b327b12e
Chandan Jadhav <119391943+JadhavC07@users.noreply.github.com>
2024-09-14 12:31:51 +0000

    added blog page

--- src/app/portfolio/blogs/page.jsx
+++ src/app/portfolio/blogs/page.jsx
@@ -1,7 +1,7 @@
 const Blog =() => {
 
     return(
-        <div>
+        <div className="flex justify-center items-center">
             <h1>This Page is under Construction </h1>
         </div>
     )
