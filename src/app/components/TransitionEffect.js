b98bf854121705638e8d7a4cfc3c4bc4a1c78301
Chandan Jadhav <119391943+JadhavC07@users.noreply.github.com>
2024-09-14 12:44:34 +0000

    added blog page

--- src/app/components/TransitionEffect.js
+++ src/app/components/TransitionEffect.js
@@ -3,25 +3,30 @@ import { motion } from "framer-motion";
 const TransitionEffect = () => {
   return (
     <>
+      {/* First layer */}
       <motion.div
-        className="fixed top-full bottom-0 right-0 w-screen h-screen z-30 bg-lime-600"
-        initial={{ y: "100%", width: "100%" }}
-        animate={{ y: "0%", width: "0%" }}
-        exit={{ y: ["0%", "100%"], width: ["0%", "100%"] }}
+        className="fixed top-0 bottom-0 right-0 w-screen h-screen z-30 bg-lime-600"
+        initial={{ y: "-100%" }} // Start from above the viewport
+        animate={{ y: "0%" }} // Slide to the viewport
+        exit={{ y: "100%" }} // Exit to below the viewport
         transition={{ duration: 0.8, ease: "easeInOut" }}
       />
+
+      {/* Second layer */}
       <motion.div
-        className="fixed top-full bottom-0 right-0 w-screen h-screen z-20 bg-lime-500"
-        initial={{ y: "100%", width: "100%" }}
-        animate={{ y: "0%", width: "0%" }}
-        exit={{ y: ["0%", "100%"], width: ["0%", "100%"] }}
+        className="fixed top-0 bottom-0 right-0 w-screen h-screen z-20 bg-lime-500"
+        initial={{ y: "-100%" }} // Start from above the viewport
+        animate={{ y: "0%" }} // Slide to the viewport
+        exit={{ y: "100%" }} // Exit to below the viewport
         transition={{ delay: 0.2, duration: 0.8, ease: "easeInOut" }}
       />
+
+      {/* Third layer */}
       <motion.div
-        className="fixed top-full bottom-0 right-0 w-screen h-screen z-10 bg-lime-300"
-        initial={{ y: "100%", width: "100%" }}
-        animate={{ y: "0%", width: "0%" }}
-        exit={{ y: ["0%", "100%"], width: ["0%", "100%"] }}
+        className="fixed top-0 bottom-0 right-0 w-screen h-screen z-10 bg-lime-300"
+        initial={{ y: "-100%" }} // Start from above the viewport
+        animate={{ y: "0%" }} // Slide to the viewport
+        exit={{ y: "100%" }} // Exit to below the viewport
         transition={{ delay: 0.4, duration: 0.8, ease: "easeInOut" }}
       />
     </>
