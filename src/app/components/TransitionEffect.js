7cbd46a96ea515a29cdfefd0a05ddb398b37802d
Chandan Jadhav <119391943+JadhavC07@users.noreply.github.com>
2024-09-14 12:46:26 +0000

    added blog page

--- src/app/components/TransitionEffect.js
+++ src/app/components/TransitionEffect.js
@@ -3,30 +3,25 @@ import { motion } from "framer-motion";
 const TransitionEffect = () => {
   return (
     <>
-      {/* First layer */}
       <motion.div
-        className="fixed top-0 bottom-0 right-0 w-screen h-screen z-30 bg-lime-600"
-        initial={{ y: "-100%" }} // Start from above the viewport
-        animate={{ y: "0%" }} // Slide to the viewport
-        exit={{ y: "100%" }} // Exit to below the viewport
+        className="fixed top-0 bottom-0 right-full w-screen h-screen z-30 bg-lime-600"
+        initial={{ x: "100%", width: "100%" }}
+        animate={{ x: "0%", width: "0%" }}
+        exit={{ x: ["0%", "100%"], width: ["0%", "100%"] }}
         transition={{ duration: 0.8, ease: "easeInOut" }}
       />
-
-      {/* Second layer */}
       <motion.div
-        className="fixed top-0 bottom-0 right-0 w-screen h-screen z-20 bg-lime-500"
-        initial={{ y: "-100%" }} // Start from above the viewport
-        animate={{ y: "0%" }} // Slide to the viewport
-        exit={{ y: "100%" }} // Exit to below the viewport
+        className="fixed top-0 bottom-0 right-full w-screen h-screen z-20 bg-lime-500"
+        initial={{ x: "100%", width: "100%" }}
+        animate={{ x: "0%", width: "0%" }}
+        exit={{ x: ["0%", "100%"], width: ["0%", "100%"] }}
         transition={{ delay: 0.2, duration: 0.8, ease: "easeInOut" }}
       />
-
-      {/* Third layer */}
       <motion.div
-        className="fixed top-0 bottom-0 right-0 w-screen h-screen z-10 bg-lime-300"
-        initial={{ y: "-100%" }} // Start from above the viewport
-        animate={{ y: "0%" }} // Slide to the viewport
-        exit={{ y: "100%" }} // Exit to below the viewport
+        className="fixed top-0 bottom-0 right-full w-screen h-screen z-10 bg-lime-300"
+        initial={{ x: "100%", width: "100%" }}
+        animate={{ x: "0%", width: "0%" }}
+        exit={{ x: ["0%", "100%"], width: ["0%", "100%"] }}
         transition={{ delay: 0.4, duration: 0.8, ease: "easeInOut" }}
       />
     </>
